9f273d4d50fa1226e1072dfdf85c2a9902bc0948
Logan Lagrange <lagrangelogan@gmail.com>
2024-01-18 21:05:09 -0500

    added get all drawing routes

--- controllers/drawingRoutes.js
+++ controllers/drawingRoutes.js
@@ -2,6 +2,15 @@ const express = require("express");
 const router = express.Router();
 const { Drawing } = require("../models"); //Imports user model
 
+// GET all drawings
+router.get(`/`,(req,res) => {
+    Drawing.findAll().then(dbDrawings => {
+        res.json(dbDrawings);
+    }).catch(err => {
+        res.status(500).json({msg:`Server Error!`, err});
+    })
+})
+
 
 
 module.exports = router;
