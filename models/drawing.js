05cd1cb8907e1d65409318e6bf31d9f6528ed12f
Logan Lagrange <lagrangelogan@gmail.com>
2024-01-18 20:00:17 -0500

    updated drawing model

--- models/drawing.js
+++ models/drawing.js
@@ -1,34 +1,21 @@
 const {Model, Datatypes} = require("sequelize");
 const sequelize = require('../config/connection');
 
-module.exports = (sequelize, DataTypes) => {
-    const Drawing = sequelize.define('Drawings', {
-      svg_id: {
-        type: DataTypes.INTEGER,
-        primaryKey: true,
-        autoIncrement: true,
-        allowNull: false
-      },
-      user_name: {
-        type: DataTypes.STRING(50),
-        allowNull: false
-      },
-      SVG_filename: {
-        type: DataTypes.STRING(50),
-        allowNull: false
-      },
-      word_id: {
-        type: DataTypes.INTEGER,
-        allowNull: false
-      }
-    }, {
-      //add xtras here
-    });
-  
-    Drawing.associate = function(models) {
-      Drawing.belongsTo(models.Users, { foreignKey: 'word_id', as: 'word' });
-    };
-  
-    return Drawing;
-  };
-  
+class Drawing extends Model {};
+
+Drawing.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+      allowNull: false
+    },
+    filename: {
+      type: DataTypes.STRING(50),
+      allowNull: false
+    }
+  }
+)
+
+module.exports = Drawing;
