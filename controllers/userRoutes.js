6e7475f4eff60e50d3058bd64a95ca9bd6c0742a
afzama <141961264+afzama@users.noreply.github.com>
2024-01-18 19:37:54 -0500

    added user Routes, get all, get one user and drawings, new user signup route

--- controllers/userRoutes.js
+++ controllers/userRoutes.js
@@ -2,11 +2,89 @@ const express = require("express");
 const router = express.Router();
 const { User } = require("../models"); //Imports user model
 const bcrypt = require("bcrypt");
+const { UUIDV4 } = require("sequelize");
 
-//login routes
+router.use(bodyParser.json());
+let Users = [];
 
-//logout routes
+//GET: Get all users 
+app.get('/users', (req, res) => {
+    res.json(Users);
+  });
 
-//signout routes
+//GET: Get one user by ID, include drawings
+app.get('/users/:id', (req, res) => {
+    const user_id = req.params.id;
+    //logic to retreive user and drawings by ID
+    const user = user.find((user) => user.id === user_id);
+    if(!user) {
+        return res.status(404).json({ error: 'User not found'});
+    }
+    const drawings = user.drawings || [];
+    res.json({user, drawings});
+});
+
+// POST: Signup route, creates a new user
+app.post('/signup', (req, res) => {
+    const newUser = req.body;
+    //Required user fields
+    if (!newUser.user_name || !newUser.user_email || !newUser.user_password) {
+        return res.status(400).json({error:'Username, email, and password are required'});
+    } 
+
+    const existingUser = user.find(
+        (user) => user.user_name === newUser.user_name || user.user_email === newUser.user_email
+    );
+
+    if (existingUser) {
+        return res.status(400).json({error: 'Username or email is already in use.'});
+    }
+    //Assign a unique ID to the new user (uuid)
+    newUser.id = uuid.v4();
+
+    //Hash password using bcrypt
+    try {
+        const hashedPassword = await bcrypt.hash(newUser.password, 10);
+        newUser.password = hashedPassword;
+
+    //Add user to the user array/database
+    users.push(newUser);
+
+    //Respond to new user
+  res.status(201).json(newUser);
+} catch (error) {
+    // Handle any errors that occur during password hashing
+    console.error('Error hashing password:', error);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
+
+// POST: Login route
+app.post('/login', (req, res) => {
+
+});
+
+// DELETE: Logout route
+app.delete('/logout', (req, res) => {
+
+});
+
+// DELETE: Delete account route
+app.delete('/users/:id', (req, res) => {
+    const userId = req.params.id;
+
+});
+
+// PUT: Adding profile picture
+app.put('/users/:id/profile-picture', (req, res) => {
+    const userId = req.params.id;
+    const profilePictureUrl = req.body.profilePictureUrl;
+
+});
+
+// Start the server
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
+  });
 
 module.exports = router;
