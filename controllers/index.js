d50d95f90691711ffef563fe61a600827801189f
Logan Lagrange <lagrangelogan@gmail.com>
2024-01-20 20:12:07 -0500

    added route handler to index.js

--- controllers/index.js
+++ controllers/index.js
@@ -7,4 +7,7 @@ router.use("/api/users", userRoutes);
 const drawingRoutes = require("./drawingRoutes");
 router.use("/api/drawings", drawingRoutes);
 
+const answerRoutes = require("./answerRoutes");
+router.use("/api/answers", answerRoutes);
+
 module.exports = router;
